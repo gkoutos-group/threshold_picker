a1c3b34c547cf591a636ad2072a16291152e5b7d
Victor R. Cardoso <labrax@users.noreply.github.com>
2021-06-25 13:07:55 +0100

    renamed methods and functions

--- cutpointr_wrapper.R
+++ cutpointr_wrapper.R
@@ -7,8 +7,8 @@
 
 
 methods_available = list('-- Manual -- '=NULL, #default if there was a movement
-                         'maximize_metric'=cutpointr::maximize_metric,
-                         'minimize_metric'=cutpointr::minimize_metric,
+                         'Maximize metric'=cutpointr::maximize_metric,
+                         'Minimize Metric'=cutpointr::minimize_metric,
                          #'maximize_loess_metric'=cutpointr::maximize_loess_metric,
                          #'minimize_loess_metric'=cutpointr::minimize_loess_metric,
                          #'maximize_spline_metric'=cutpointr::maximize_spline_metric,
@@ -18,29 +18,29 @@ methods_available = list('-- Manual -- '=NULL, #default if there was a movement
                          #'maximize_boot_metric'=cutpointr::maximize_boot_metric,
                          #'minimize_boot_metric'=cutpointr::minimize_boot_metric,
                          #'oc_manual'=cutpointr::oc_manual,
-                         'oc_mean'=cutpointr::oc_mean,
-                         'oc_median'=cutpointr::oc_median,
-                         'oc_youden_kernel'=cutpointr::oc_youden_kernel,
-                         'oc_youden_normal'=cutpointr::oc_youden_normal
+                         'Sample mean as cutpoint'=cutpointr::oc_mean,
+                         'Sample median as cutpoint'=cutpointr::oc_median,
+                         'Maximize Youden-Index after kernel smoothing'=cutpointr::oc_youden_kernel,
+                         'Maximize Youden-Index assuming normal distribution'=cutpointr::oc_youden_normal
                          )
 
 
-metrics_available = list('accuracy'=cutpointr::accuracy,
-                         'abs_d_sens_spec'=cutpointr::abs_d_sens_spec,
-                         'abs_d_ppv_npv'=cutpointr::abs_d_ppv_npv,
-                         'roc01'=cutpointr::roc01,
-                         'cohens_kappa'=cutpointr::cohens_kappa,
-                         'sum_sens_spec'=cutpointr::sum_sens_spec,
-                         'sum_ppv_npv'=cutpointr::sum_ppv_npv,
-                         'prod_sens_spec'=cutpointr::prod_sens_spec,
-                         'prod_ppv_npv'=cutpointr::prod_ppv_npv,
-                         'youden'=cutpointr::youden,
-                         'odds_ratio'=cutpointr::odds_ratio,
-                         'risk_ratio'=cutpointr::risk_ratio,
-                         'p_chisquared'=cutpointr::p_chisquared,
+metrics_available = list('Accuracy'=cutpointr::accuracy,
+                         'Absolute difference between sensitivity and specificity'=cutpointr::abs_d_sens_spec,
+                         'Absolute difference between PPV and NPV'=cutpointr::abs_d_ppv_npv,
+                         'Distance to point (0,1) in AUCROC curve'=cutpointr::roc01,
+                         'Cohens Kappa'=cutpointr::cohens_kappa,
+                         'PPV + NPV'=cutpointr::sum_ppv_npv,
+                         'PPV * NPV'=cutpointr::prod_ppv_npv,
+                         'Sensitivity + Specificity'=cutpointr::sum_sens_spec,
+                         'Sensitivity * Specificity'=cutpointr::prod_sens_spec,
+                         'Sensitivity + Specificity - 1'=cutpointr::youden,
+                         'Diagnostic Odds Ratio'=cutpointr::odds_ratio,
+                         'Relative risk'=cutpointr::risk_ratio,
+                         'P-value of Chi-Squared test'=cutpointr::p_chisquared,
                          #'cost_misclassification'=cost_misclassification,
                          #'total_utility'=cutpointr::total_utility,
-                         'F1_score'=cutpointr::F1_score
+                         'F1-score'=cutpointr::F1_score
                          #'metric_constrain'=cutpointr::metric_constrain,
                          #'sens_constrain'=cutpointr::sens_constrain,
                          #'spec_constrain'=cutpointr::spec_constrain,
